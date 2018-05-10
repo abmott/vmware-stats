c15884db2b046375c905408c03322a8114b82608
abmott <adam.mott@csaa.com>
2018-05-10 08:27:36 -0700

    changed to three jobs

--- ci/tasks/get_esx_info/vmware-metrics.rb
+++ ci/tasks/get_esx_info/vmware-metrics.rb
@@ -24,89 +24,67 @@ def datadogmetric(label1, label2, host, metric, env)
              https://app.datadoghq.com/api/v1/series?api_key=#{ENV['DATADOG_API_KEY']}`
 end
 
-pcf_envs = ["sandbox", "gdc", "pdc"]
-
-pcf_envs.each do |test|
-  puts "#{ENV["#{test.upcase}_ESX_HOST"]}"
-end
-
-pcf_envs.each do |pcf_env|
-puts "starting to gather for #{pcf_env}"
-esx_host = "#{pcf_env.upcase}_ESX_HOST"
-puts esx_host
-puts "#{ENV['SANDBOX_ESX_HOST']}"
-puts "#{ENV['SANDBOX_ESX_HOST']}"
-end
 ##Connect to vCenter Variables
-#v = RbVmomi::VIM
-#vim = v.connect host: "#{ENV["#{esx_host}"]}", insecure: true, user: "#{ENV["#{pcf_env.upcase}_ESX_USER"]}", password: "#{ENV["#{pcf_env.upcase}_ESX_PASSWORD"]}"
-#dc = vim.serviceInstance.find_datacenter path="Digital Services"
-#
-##pull ESX Host stats
-#dc.hostFolder.children.first.host.each do |host|
-#  puts "-----------------------------"
-#  puts host.summary.config.name.downcase
-#  puts "Logical Processors: #{host.hardware.cpuInfo.numCpuCores.to_i * 2}"
-#  cpucapacity = 45.88
-#  cpuused = (host.summary.quickStats.overallCpuUsage.to_f/1000).round(1)
-#  cpufree = (45.88-(host.summary.quickStats.overallCpuUsage.to_f/1000)).round(1)
-#  cpuusedperc = ((((host.summary.quickStats.overallCpuUsage.to_f/1000))/45.88)*100).round(2)
-#  cpufreeperc = (100-((((host.summary.quickStats.overallCpuUsage.to_f/1000))/45.88)*100)).round(2)
-#  memcap = bytetoGB(host.hardware.memorySize.to_i)
-#  memuse = MBtoGB(host.summary.quickStats.overallMemoryUsage.to_i)
-#  memfree = memcap - memuse
-#  memfreeperc = ((memuse.to_f/memcap.to_f)*100).round(2)
-#  memusedperc = (100-((memuse.to_f/memcap.to_f)*100)).round(2)
-#  puts "CPU Capacity: #{cpucapacity} GHz"
-#  puts "CPU Used: #{cpuused} GHz"
-#  puts "CPU Free: #{cpufree} GHz"
-#  puts "CPU Used: #{cpuusedperc} %"
-#  puts "CPU Free: #{cpufreeperc} %"
-#  puts "Memory Capacity: #{memcap} GB"
-#  puts "Memory Used: #{memuse} GB"
-#  puts "Memory Free: #{memfree} GB"
-#  puts "Memory Used: #{memusedperc} %"
-#  puts "Memory Free: #{memfreeperc} %"
-#  puts "posting metrics to datadog started"
-#  datadogmetric("cpu", "capacity", host.summary.config.name.downcase, cpucapacity, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("cpu", "used", host.summary.config.name.downcase, cpuused, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("cpu", "free", host.summary.config.name.downcase, cpufree, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("cpu", "percent_used", host.summary.config.name.downcase, cpuusedperc, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("cpu", "percent_free", host.summary.config.name.downcase, cpufreeperc, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("memory", "capacity", host.summary.config.name.downcase, memcap, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("memory", "used", host.summary.config.name.downcase, memuse, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("memory", "free", host.summary.config.name.downcase, memfree, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("memory", "percent_used", host.summary.config.name.downcase, memusedperc, "#{eval("#{pcf_env}_pcf_tag")}")
-#  datadogmetric("memory", "percent_free", host.summary.config.name.downcase, memfreeperc, "#{eval("#{pcf_env}_pcf_tag")}")
-#  puts "posting metrics to datadog completed"
-#end
-#end
-#
-##free space of Datastore (single datastore)
-#pcf_envs.each do |pcf_env|
-#puts "starting to gather for #{pcf_env}"
-#
-#v = RbVmomi::VIM
-#vim = v.connect host: "#{eval("#{pcf_env}_esx_host")}", insecure: true, user: "#{eval("#{pcf_env}_esx_user")}", password: "#{eval("#{pcf_env}_esx_password")}"
-#dc = vim.serviceInstance.find_datacenter path="#{eval("#{pcf_env}_esx_datacenter")}"
-#
-#puts "===================================="
-#ds_array = "#{eval("#{pcf_env}_datastores")}"
-#ds_array.split(',').each do |datastore|
-#  puts "------------------------------------"
-#  ds = dc.find_datastore name = "#{datastore}"
-# puts "#{datastore}"
-# dscapacity = bytetoGB(ds.summary.capacity.to_i)
-# dsused = bytetoGB(ds.summary.capacity.to_i) - bytetoGB(ds.summary.freeSpace.to_i)
-# dsfree = bytetoGB(ds.summary.freeSpace.to_i)
-# puts "Datastore Capacity: #{dscapacity} GB"
-# puts "Datastore Used: #{dsused} GB"
-# puts "Datastore Free: #{dsfree} GB"
-# puts "posting metics to datadog started"
-# datadogmetric("storage", "capacity", datastore, dscapacity, "#{eval("#{pcf_env}_pcf_tag")}")
-# datadogmetric("storage", "used", datastore, dsused, "#{eval("#{pcf_env}_pcf_tag")}")
-# datadogmetric("storage", "free", datastore, dsfree, "#{eval("#{pcf_env}_pcf_tag")}")
-# puts "posting metrics to datadog completed"
-#end
-#end
-#
+v = RbVmomi::VIM
+vim = v.connect host: "#{ENV['ESX_HOST']}", insecure: true, user: "#{ENV['ESX_USER']}", password: "#{ENV['ESX_PASSWORD']}"
+dc = vim.serviceInstance.find_datacenter path="#{ENV['ESX_DATACENTER']}"
+
+#pull ESX Host stats
+dc.hostFolder.children.first.host.each do |host|
+  puts "-----------------------------"
+  puts host.summary.config.name.downcase
+  puts "Logical Processors: #{host.hardware.cpuInfo.numCpuCores.to_i * 2}"
+  cpucapacity = 45.88
+  cpuused = (host.summary.quickStats.overallCpuUsage.to_f/1000).round(1)
+  cpufree = (45.88-(host.summary.quickStats.overallCpuUsage.to_f/1000)).round(1)
+  cpuusedperc = ((((host.summary.quickStats.overallCpuUsage.to_f/1000))/45.88)*100).round(2)
+  cpufreeperc = (100-((((host.summary.quickStats.overallCpuUsage.to_f/1000))/45.88)*100)).round(2)
+  memcap = bytetoGB(host.hardware.memorySize.to_i)
+  memuse = MBtoGB(host.summary.quickStats.overallMemoryUsage.to_i)
+  memfree = memcap - memuse
+  memfreeperc = ((memuse.to_f/memcap.to_f)*100).round(2)
+  memusedperc = (100-((memuse.to_f/memcap.to_f)*100)).round(2)
+  puts "CPU Capacity: #{cpucapacity} GHz"
+  puts "CPU Used: #{cpuused} GHz"
+  puts "CPU Free: #{cpufree} GHz"
+  puts "CPU Used: #{cpuusedperc} %"
+  puts "CPU Free: #{cpufreeperc} %"
+  puts "Memory Capacity: #{memcap} GB"
+  puts "Memory Used: #{memuse} GB"
+  puts "Memory Free: #{memfree} GB"
+  puts "Memory Used: #{memusedperc} %"
+  puts "Memory Free: #{memfreeperc} %"
+  puts "posting metrics to datadog started"
+  datadogmetric("cpu", "capacity", host.summary.config.name.downcase, cpucapacity, "#{ENV['PCF_TAG']}")
+  datadogmetric("cpu", "used", host.summary.config.name.downcase, cpuused, "#{ENV['PCF_TAG']}")
+  datadogmetric("cpu", "free", host.summary.config.name.downcase, cpufree, "#{ENV['PCF_TAG']}")
+  datadogmetric("cpu", "percent_used", host.summary.config.name.downcase, cpuusedperc, "#{ENV['PCF_TAG']}")
+  datadogmetric("cpu", "percent_free", host.summary.config.name.downcase, cpufreeperc, "#{ENV['PCF_TAG']}")
+  datadogmetric("memory", "capacity", host.summary.config.name.downcase, memcap, "#{ENV['PCF_TAG']}")
+  datadogmetric("memory", "used", host.summary.config.name.downcase, memuse, "#{ENV['PCF_TAG']}")
+  datadogmetric("memory", "free", host.summary.config.name.downcase, memfree, "#{ENV['PCF_TAG']}")
+  datadogmetric("memory", "percent_used", host.summary.config.name.downcase, memusedperc, "#{ENV['PCF_TAG']}")
+  datadogmetric("memory", "percent_free", host.summary.config.name.downcase, memfreeperc, "#{ENV['PCF_TAG']}")
+  puts "posting metrics to datadog completed"
+end
+
+#free space of Datastore (single datastore)
+puts "===================================="
+ds_array = "#{ENV['PCF_TAG']}"
+ds_array.split("''")
+ds_array.each do |datastore|
+  puts "------------------------------------"
+  ds = dc.find_datastore name = "#{datastore}"
+ puts "#{datastore}"
+ dscapacity = bytetoGB(ds.summary.capacity.to_i)
+ dsused = bytetoGB(ds.summary.capacity.to_i) - bytetoGB(ds.summary.freeSpace.to_i)
+ dsfree = bytetoGB(ds.summary.freeSpace.to_i)
+ puts "Datastore Capacity: #{dscapacity} GB"
+ puts "Datastore Used: #{dsused} GB"
+ puts "Datastore Free: #{dsfree} GB"
+ puts "posting metics to datadog started"
+ datadogmetric("storage", "capacity", datastore, dscapacity, "#{ENV['PCF_TAG']}")
+ datadogmetric("storage", "used", datastore, dsused, "#{ENV['PCF_TAG']}")
+ datadogmetric("storage", "free", datastore, dsfree, "#{ENV['PCF_TAG']}")
+ puts "posting metrics to datadog completed"
+end
